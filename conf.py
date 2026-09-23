b12db5a01e2a18f8f055283154259a4d849869d5
Coulter <jcoulter8@MacBook-Pro-2.local>
2026-09-23 16:14:47 -0400

    EC: back to furo for now
    Signed-off-by: Coulter <jcoulter8@MacBook-Pro-2.local>

--- conf.py
+++ conf.py
@@ -17,40 +17,40 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
-html_theme = 'sphinxawesome_theme'
+html_theme = 'furo'
 html_title = "GT Nexus Docs"
 
 html_static_path = ['_static']
-html_css_files = ['branding.css']
+html_css_files = ['custom.css']
 
 html_theme_options = {
     'source_repository': 'https://github.com/gt-nexus/nexus-docs',
     'source_branch': 'main',
     'source_directory': '/',
     'navigation_with_keys': True,
-    "logo_light": "_static/horizontal-lightmode.svg",
-    "logo_dark": "_static/horizontal-darkmode.svg"
+    #"logo_light": "_static/horizontal-lightmode.svg",
+    #"logo_dark": "_static/horizontal-darkmode.svg"
+    "light_css_variables": {
+        # Sidebar and Top Bar
+        "color-brand-primary": "#003057",    # GT Navy for navigation elements
+        "color-brand-content": "#857437",    # Accessible Dark Gold for links/content
+        
+        # Sidebar background and text
+        "color-sidebar-background": "#f9f6e5", # "Diploma" (GT light ivory)
+        "color-sidebar-link-text": "#003057",
+        
+        # Admonitions (Notes, Tips, etc.)
+        "color-admonition-title--note": "#B3A369", # Tech Gold
+        "color-admonition-title-background--note": "rgba(179, 163, 105, 0.1)",
+    },
+    "dark_css_variables": {
+        "color-brand-primary": "#B3A369",    # Gold stands out better in dark mode
+        "color-brand-content": "#BFB37C",    # Lighter gold for better dark contrast
+        
+        "color-sidebar-background": "#002233", # "Atlanta Fog" (GT dark blue-black)
+        "color-sidebar-link-text": "#FFFFFF",
+    },
 }
-#    "light_css_variables": {
-#        # Sidebar and Top Bar
-#        "color-brand-primary": "#003057",    # GT Navy for navigation elements
-#        "color-brand-content": "#857437",    # Accessible Dark Gold for links/content
-#        
-#        # Sidebar background and text
-#        "color-sidebar-background": "#f9f6e5", # "Diploma" (GT light ivory)
-#        "color-sidebar-link-text": "#003057",
-#        
-#        # Admonitions (Notes, Tips, etc.)
-#        "color-admonition-title--note": "#B3A369", # Tech Gold
-#        "color-admonition-title-background--note": "rgba(179, 163, 105, 0.1)",
-#    },
-#    "dark_css_variables": {
-#        "color-brand-primary": "#B3A369",    # Gold stands out better in dark mode
-#        "color-brand-content": "#BFB37C",    # Lighter gold for better dark contrast
-#        
-#        "color-sidebar-background": "#002233", # "Atlanta Fog" (GT dark blue-black)
-#        "color-sidebar-link-text": "#FFFFFF",
-#    },
 
 # Adding the GT Interlocking Logo 
 #html_logo = "_static/horizontal-lightmode.svg" 
