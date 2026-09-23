5228b23e66c082db379b1ceeb67322cd04b67cef
Coulter <jcoulter8@MacBook-Pro-2.local>
2026-09-23 16:20:15 -0400

    EC: bad varnames
    Signed-off-by: Coulter <jcoulter8@MacBook-Pro-2.local>

--- conf.py
+++ conf.py
@@ -28,8 +28,8 @@ html_theme_options = {
     'source_branch': 'main',
     'source_directory': '/',
     'navigation_with_keys': True,
-    "logo_light": "horizontal-lightmode.svg",
-    "logo_dark": "horizontal-darkmode.svg"
+    "light_logo": "horizontal-lightmode.svg",
+    "dark_logo": "horizontal-darkmode.svg"
     "light_css_variables": {
         # Sidebar and Top Bar
         "color-brand-primary": "#003057",    # GT Navy for navigation elements
